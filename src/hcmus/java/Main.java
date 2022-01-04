cb1e4d52f88d9c44db237415948b1f3d8fb1a296
tuilakie <ntneik15@gmail.com>
2022-01-04 23:15:48 +0700

    commit cho đủ 10 commit

--- src/hcmus/java/Main.java
+++ src/hcmus/java/Main.java
@@ -5,6 +5,7 @@ import hcmus.java.Views.MainView;
 public class Main {
     public static void main(String[] args) {
         Slang.getInstance().LoadData("data.txt");
+
         MainView.getInstance().createAndShowGUI();
     }
 }
